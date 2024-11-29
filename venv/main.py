016f42b07b16fa15e89cc1a3b5626e28bafba2a3
Hector Jairo Reyes <hectorjrey09@gmail.com>
2024-11-29 16:51:20 -0500

    Update main.py

--- venv/main.py
+++ venv/main.py
@@ -15,27 +15,27 @@ st.dataframe(table)
 st.image("https://picsun.photos/200/300")
 
 
-# st.markdown("# Esto es un Titulo")
-# st.markdown("## Esto es codigo")
-# st.markdown("### Esto es codigo")
-# st.markdown("**Codigo** Prueba")
-# st.markdown("*Codigo* Prueba")
-# st.markdown("> Codigo Prueba")
-# st.markdown("1. Item\n 2. Item")
-# str= "print('Hello World')"
-# st.code(str)
-# st.markdown("_ _ _")
-# st.markdown('[Google](https://google.com)')
-# table = '''
+st.markdown("# Esto es un Titulo")
+st.markdown("## Esto es codigo")
+st.markdown("### Esto es codigo")
+st.markdown("**Codigo** Prueba")
+st.markdown("*Codigo* Prueba")
+st.markdown("> Codigo Prueba")
+st.markdown("1. Item\n 2. Item")
+str= "print('Hello World')"
+st.code(str)
+st.markdown("_ _ _")
+st.markdown('[Google](https://google.com)')
+table = '''
 # | Syntax | Description |
 # | ----------- | ----------- |
 # | Header | Title |
 # | Paragraph | Text |
 
 # '''
-# st.markdown(table)
-# json = {"a":"1,2,3","b":"4,5,6"}
-# st.json(json)
+st.markdown(table)
+json = {"a":"1,2,3","b":"4,5,6"}
+st.json(json)
 st.metric(label='win speed',
           value='70 ms', delta='5.7')
 
